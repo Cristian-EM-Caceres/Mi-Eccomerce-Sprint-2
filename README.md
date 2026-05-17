6b41809c0327199fc06711f5d45e6f44d312353e
Ezequiel Vilchez <vilchezezequiel9@gmail.com>
2026-05-17 18:43:45 -0300

    Migracion de la Base del TP1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Mi-Eccomerce-Sprint-2
 Segundo trabajo práctico de la materia "Web 1" de la carrera "Programación Universitaria de Sistemas" de la "Universidad Nacional de Villa Mercedes".
+
+
---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('pages/login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('pages/register');
+});
+
+
+app.get('/index', (req, res) => {
+    res.render('pages/index');
+});
+
+
+app.listen(3000, () => console.log("Server en linea 🫡"));
